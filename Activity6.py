16e5345ac032a74cb735226d0894f59376bd5c01
karlamz2007 <karlabulencia2007@gmail.com>
2025-08-18 03:55:23 +0800

    Update Activity6.py

--- Activity6.py
+++ Activity6.py
@@ -1,1 +1,37 @@
+firstnum = eval (input("Input a number  : "))
+secondnum = eval (input("Input another number  : "))
+answer = firstnum + secondnum
+print ("The answer sum : \n ", answer)
+
+firstnum = eval (input("Input a number  : "))
+secondnum = eval (input("Input another number  : "))
+answer = firstnum - secondnum
+print ("The difference is : \n ", answer)
+
+firstnum = eval (input("Input a number  : "))
+secondnum = eval (input("Input another number  : "))
+answer = firstnum * secondnum
+print ("The product is : \n ", answer)
+
+firstnum = eval (input("Input a number  : "))
+secondnum = eval (input("Input another number  : "))
+answer = firstnum / secondnum
+print ("The quotient is : \n ", answer)
+
+
+firstnum = eval (input("Input a number  : "))
+secondnum = eval (input("Input another number  : "))
+answer = firstnum // secondnum
+print ("The answer is : \n ", answer)
+
+firstnum = eval (input("Input a number  : "))
+secondnum = eval (input("Input another number  : "))
+answer = firstnum & secondnum
+print ("The answer is : \n ", answer)
+
+firstnum = eval (input("Input a number  : "))
+secondnum = eval (input("Input another number  : "))
+answer = firstnum ** secondnum
+print ("The answer is : \n ", answer)
+
 
