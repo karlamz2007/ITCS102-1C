1827fd840c80b1f35cce99eb6ac3462dc4d51dea
karlamz2007 <karlabulencia2007@gmail.com>
2025-08-18 03:45:29 +0800

    Create codechallenge_1.py

---
+++ codechallenge_1.py
@@ -0,0 +1,8 @@
+yourname = input ("Write
+your name -=--› ")
+print ("\t\t\t\t\t\t\t\t\t\t\t\t\t *\n\t\t\t\t\t\t\t\t\t\t\t\t*\t\t*\n\t\t\t\t\t\t\t\t\t\t\t*\t\t
+\t\t*\n\t\t\t\t\t\t\t\t\t\t*\t\tHii\t\t\t
+\t*\n\t\t\t\t\t\t\t\t\t*\t\t\t\t", yourname ,"\t\t\t
+\t*\n\t\t\t\t\t\t\t\t\t\t*\t\t\t\t\t\t*\n
+(t\t\t*\t\t\t\t*\n\t\t\t\t\t\t\t\t\t\t\t
+\t*\t\t*\n\t\t\t\t\t\t\t\t\t\t\t\t\t*")
