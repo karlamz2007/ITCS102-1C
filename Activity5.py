038b8e57b2e7cae2a5aad1bfc949f3f438700d4d
karlamz2007 <karlabulencia2007@gmail.com>
2025-08-18 03:50:34 +0800

    Update Activity5.py

--- Activity5.py
+++ Activity5.py
@@ -1,2 +1,8 @@
 name = input("Type anything there ..... ")
 print ("The data type of name is", type (name) )
+
+name = eval(input ("Type anything.... "))
+print ("The data type of name is ", type (name))
+
+x = eval(input("Give me a number : "))
+print ("The number you've input is called " , type (x))
