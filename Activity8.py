9399fa9ce1499634a1dd42e0f48ac81e532f45fc
karlamz2007 <karlabulencia2007@gmail.com>
2025-08-18 14:22:19 +0800

    Create Activity8.py

---
+++ Activity8.py
@@ -0,0 +1,12 @@
+
+
+thing = input ("Give me a word --- > ")
+thing1 = input ("Put another word that sounds like the first word you've input --- > ")
+
+print (thing != thing1)
+
+fname = input ("Input your first name : ")
+lname = input ("Input your last name : ")
+
+fname += lname
+print (fname)
