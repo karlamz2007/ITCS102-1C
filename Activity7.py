acb626762409d97f0da28f738d1985f0274f112a
karlamz2007 <karlabulencia2007@gmail.com>
2025-08-18 14:21:42 +0800

    Create Activity7.py

---
+++ Activity7.py
@@ -0,0 +1,20 @@
+x = 8
+
+print ("The value of x is -- > ", x )
+
+x += 5
+print ("The new value of x is --- > ", x )
+
+x += 7
+x += 18 
+x -= 4
+x *= 4
+x %= 6
+
+print ("The updated value of x is --- > ", x)
+
+solve = eval(input("Tell me a number : " ))
+print ("The number you input is interesting")
+solve1 = eval(input("Put a another number so that we'll add the numbers "))
+x = solve + solve1 
+print ("The final number is --- > " , x )
