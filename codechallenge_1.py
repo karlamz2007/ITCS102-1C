27b8f4f0d3883cbbcc8308d2ec539e479bd1883c
karlamz2007 <karlabulencia2007@gmail.com>
2025-08-18 13:28:06 +0800

    codechallenge_1.py

--- codechallenge_1.py
+++ codechallenge_1.py
@@ -1,8 +1,2 @@
-yourname = input ("Write
-your name -=--› ")
-print ("\t\t\t\t\t\t\t\t\t\t\t\t\t *\n\t\t\t\t\t\t\t\t\t\t\t\t*\t\t*\n\t\t\t\t\t\t\t\t\t\t\t*\t\t
-\t\t*\n\t\t\t\t\t\t\t\t\t\t*\t\tHii\t\t\t
-\t*\n\t\t\t\t\t\t\t\t\t*\t\t\t\t", yourname ,"\t\t\t
-\t*\n\t\t\t\t\t\t\t\t\t\t*\t\t\t\t\t\t*\n
-(t\t\t*\t\t\t\t*\n\t\t\t\t\t\t\t\t\t\t\t
-\t*\t\t*\n\t\t\t\t\t\t\t\t\t\t\t\t\t*")
+yourname = input ("Write your name ----> ")
+print ("\t\t\t\t\t\t\t\t\t\t\t\t\t * \n\t\t\t\t\t\t\t\t\t\t\t\t*\t\t*\n\t\t\t\t\t\t\t\t\t\t\t*\t\t\t\t*\n\t\t\t\t\t\t\t\t\t\t*\t\tHii\t\t\t\t*\n\t\t\t\t\t\t\t\t\t*\t\t\t\t",yourname,"\t\t\t\t*\n\t\t\t\t\t\t\t\t\t\t*\t\t\t\t\t\t*\n\t\t\t\t\t\t\t\t\t\t\t*\t\t\t\t*\n\t\t\t\t\t\t\t\t\t\t\t\t*\t\t*\n\t\t\t\t\t\t\t\t\t\t\t\t\t*")
