61454b8f3b9f4b5a6332a23c65d3abfd9c1085f5
karlamz2007 <karlabulencia2007@gmail.com>
2025-08-27 13:04:39 +0800

    Create Activity9.py

---
+++ Activity9.py
@@ -0,0 +1,15 @@
+f1 = 5
+f2 = 2
+
+r1 = 17
+r2 = 21
+
+print ( (f1 > f2) and (r1 < r2) )
+print ( (f1 == f2) or (r1 > r2) )
+print ( not ((f1 == f2) or (r1 > r2)) )
+
+ign = 'Karlamz'
+ps = 'pass99word11'
+
+print ("Login Successful ^ ^ ")
+print ( (ign == 'Karlamz') and ( ps == 'pass99word11'))
