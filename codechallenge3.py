9f1dc19b62938cc77386658b40c9cd0f61244919
karlamz2007 <karlabulencia2007@gmail.com>
2025-10-27 13:37:50 +0800

    Create codechallenge3.py

---
+++ codechallenge3.py
@@ -0,0 +1,13 @@
+name = input("What is your name : ") 
+print ("Hello goodmorning/goodafternoon", name )
+bayad = input ("how much is your fare fee ? ")
+ask = input ("are you a student ? Yes/No ") 
+
+if (ask == 'Yes') or (ask == 'YES'):
+	print ("You have been discounted 20%, your fare fee is 24") 
+else :
+	print("Your going to pay the regular fare fee which is 30")
+
+
+
+
